0a7d469d0233300d9f79db6b9dd1de943905f238
dheeraj.suthar <dheeraj.suthar@snapdeal.com>
2022-09-27 14:47:58 +0530

    ng chrome scripts added.

---
+++ scripts/webpack.config.js
@@ -0,0 +1,28 @@
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const { join } = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    contentPage: join(__dirname, 'contentPage.ts'),
+    serviceWorker: join(__dirname, 'serviceWorker.ts')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    path: join(__dirname, '../../dist'),
+    filename: '[name].js'
+  },
+  plugins: [new CheckerPlugin()],
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
